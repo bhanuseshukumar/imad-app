0a1ce4b40b4a6e34655850e8ff62966ee118cf4f
bhanuseshukumar <30802522+bhanuseshukumar@users.noreply.github.com>
2017-08-08 21:11:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,rest){
-    res.send('artcle-one is up and running.');
+app.get('/article-one', function(req, rest){
+    res.send('artcle-one is up and running');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
